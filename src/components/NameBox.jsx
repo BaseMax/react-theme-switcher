f7142e7f1e1cddb419162477aa6c4bb61be2cda0
Max Base <MaxBaseCode@Gmail.Com>
2020-12-12 10:34:08 +0330

    Create NameBox.jsx

---
+++ src/components/NameBox.jsx
@@ -0,0 +1,20 @@
+// import React from "react";
+import React, { useState, useContext } from "react";
+import {ThemeContext} from "../providers/ThemeProvider";
+import { THEME_TYPE } from "../constants";
+
+const NameBox = () => {
+  const [themeMode, setTheme] = useContext(ThemeContext);
+  return (
+    <div className="box">
+      <span className="text-color">
+      	{/*
+      	themeMode === THEME_TYPE.DARK ? "DARK" : "LIGHT"
+      	*/ }
+      	Lorem ipsum, dolor sit amet consectetur adipisicing elit
+      </span>
+    </div>
+  );
+};
+
+export default NameBox;
